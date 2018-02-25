a42651061d3f79fffb85b32a9d5feed9422cf07d
LevFlavien <levesque.flavien@outlook.fr>
2018-02-26 00:34:29 +0100

    install fix

--- install.sh
+++ install.sh
@@ -15,6 +15,7 @@ echo Récupération du projet...
 curl https://codeload.github.com/LevFlavien/appli_vaisseau_satellite/tar.gz/test -o satellite.tar.gz
 tar -zxvf satellite.tar.gz
 cd appli_vaisseau_satellite-test
+rm install.sh
 
 # composer installation
 echo Installation de composer...
